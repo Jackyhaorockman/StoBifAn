29139e11bbef829ed0da22a5bcad5733f886a04b
Shuohao Liao <liao@maths.ox.ac.uk>
2014-08-10 22:40:36 +0100

    Update

--- ParametricAnalysis/parameter_cost_function.m
+++ ParametricAnalysis/parameter_cost_function.m
@@ -1,4 +1,4 @@
-function output = parameter_cost_function(input_tensor, mu_order, beta, mu_hat, m_index, d, x_lim)
+function output = parameter_cost_function(tt_moment, tt_integral, beta, mu_hat, p_index, d, h)
 % PARAMETER_COST_FUNCTION Compute the cost function for parameter
 % estimation using the method of moments from the tensorised solution,
 % input_tensor. The cost function will be a weighted sum of the difference
@@ -22,8 +22,25 @@ function output = parameter_cost_function(input_tensor, mu_order, beta, mu_hat, 
 % -----------------------------
 
 
-[n_m, n_x] = size(mu_order)
+% compute the tt index for the location specified by p_index
+ind = [];
 
-% For normalisation purpose, we need to first compute the integral. 
-output = tt_moment_full( input_t, zeros(1,n_x), m_index, d, x_lim, 0);
+for i = 1 : length(d)
+    
+    ind = [ind, tt_sub2ind(2*ones(1,d(i)), p_index(i))];
+    
+end
+
+% compute the cost function
+J = 0;
+
+for i = 1 : length(beta)
+    
+    mu = tt_moment{i}(ind) / tt_integral(ind) / h;
+    
+    J = J + beta(i)*(mu - mu_hat(i))^2 / mu_hat(i)^2;
+    
+end
+
+output = J;
 
